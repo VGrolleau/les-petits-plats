945b3476baab49a81a0f90b655d23e816f35e15a
VGrolleau <virginie.grolleau+github@protonmail.com>
2022-06-26 11:03:48 +0200

    Get recipe cards with their dynamic information

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,18 @@
 import { recipes } from "../data/recipes.js";
-
 // console.log(recipes);
-recipes.forEach(recipe => {
-    // console.log(recipe);
-    let recipeModel = new Recipe(recipe);
-    recipeModel.getRecipe();
-});
+
+function displayData(recipes) {
+    const recipesSection = document.querySelector(".recipes-section");
+    recipes.forEach(recipe => {
+        // console.log(recipe);
+        const recipeModel = new Recipe(recipe);
+        const recipeCardDOM = recipeModel.getRecipe();
+        recipesSection.appendChild(recipeCardDOM);
+    });
+}
+
+function init() {
+    displayData(recipes);
+}
+
+init();
--- scripts/models/Recipe.js
+++ scripts/models/Recipe.js
@@ -5,12 +5,73 @@ class Recipe {
         this.servings = data.servings;
         this.ingredients = data.ingredients;
         this.time = data.time;
+        this.description = data.description;
         this.appliance = data.appliance;
         this.ustensils = data.ustensils;
+        this.img = `https://picsum.photos/400?random=${this.idRecipe}`;
     }
 
     getRecipe() {
-        console.log('id :', this.idRecipe, '/', this.name, '/ portions :', this.servings, '/ ingrédients :', this.ingredients, '/', this.time, 'min / appareil :', this.appliance, '/ ustensiles :', this.ustensils);
-        console.log(this);
+        // console.log('id :', this.idRecipe, '/', this.name, '/ portions :', this.servings, '/ ingrédients :', this.ingredients, '/', this.time, 'min / appareil :', this.appliance, '/ ustensiles :', this.ustensils);
+        // console.log(this);
+
+        const recipeCard = document.createElement('article');
+        recipeCard.classList.add("recipe-card");
+
+        const img = document.createElement('img');
+        img.setAttribute("src", this.img);
+        img.setAttribute("alt", this.name);
+
+        const recipeContent = document.createElement('div');
+        recipeContent.classList.add("recipe-content");
+
+        const recipeTitleTime = document.createElement('div');
+        recipeTitleTime.classList.add("recipe-title-time");
+
+        const recipeTitle = document.createElement('h2');
+        recipeTitle.classList.add("recipe-title");
+        recipeTitle.textContent = this.name;
+
+        const recipeTime = document.createElement('p');
+        recipeTime.classList.add("recipe-time");
+        recipeTime.innerHTML = `<i class="fa-regular fa-clock"></i> <span class="time">${this.time}</span> min`;
+
+        recipeTitleTime.appendChild(recipeTitle);
+        recipeTitleTime.appendChild(recipeTime);
+
+        const recipeIngredientsDescription = document.createElement('div');
+        recipeIngredientsDescription.classList.add("recipe-ingredients-description");
+
+        const ingredients = document.createElement('ul');
+        ingredients.classList.add("ingredients");
+
+        this.ingredients.forEach(ingredient => {
+            let ingredientLi = document.createElement('li');
+            if (!ingredient.unit) {
+                ingredientLi.innerHTML = `<span class="ingredient">${ingredient.ingredient}</span> : <span class="quantity">${ingredient.quantity}</span>`;
+            }
+            if (!ingredient.quantity) {
+                ingredientLi.innerHTML = `<span class="ingredient">${ingredient.ingredient}</span>`;
+            }
+            if (ingredient.unit) {
+                ingredientLi.innerHTML = `<span class="ingredient">${ingredient.ingredient}</span> : <span class="quantity">${ingredient.quantity}</span> <span class="unit">${ingredient.unit}</span>`;
+            }
+            ingredients.appendChild(ingredientLi);
+        });
+
+        const description = document.createElement('p');
+        description.classList.add("description");
+        description.textContent = this.description;
+
+        recipeIngredientsDescription.appendChild(ingredients);
+        recipeIngredientsDescription.appendChild(description);
+
+        recipeContent.appendChild(recipeTitleTime);
+        recipeContent.appendChild(recipeIngredientsDescription);
+
+        recipeCard.appendChild(img);
+        recipeCard.appendChild(recipeContent);
+
+        return recipeCard;
     }
 }
